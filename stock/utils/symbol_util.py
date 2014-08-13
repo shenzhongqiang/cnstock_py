f39175674fe2d48b91621f39bea3b078bfa43802
Zhongqiang Shen <zqshen.1985@gmail.com>
2014-08-13 10:19:39 +0800

    symbol utilities

--- stock/utils/symbol_util.py
+++ stock/utils/symbol_util.py
@@ -1,22 +1,40 @@
 import os.path
 import re
-import json
-import pprint
 from stock.utils.request import *
+
+class InvalidType(Exception):
+    pass
 
 SYMDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
     '../../data/symbol')
-ALL_SYM = os.path.join(SYMDIR, 'symbols.all')
-SH_SYM = os.path.join(SYMDIR, 'symbols.sh')
-SZ_SYM = os.path.join(SYMDIR, 'symbols.sz')
-CY_SYM = os.path.join(SYMDIR, 'symbols.cy')
-ID_SYM = os.path.join(SYMDIR, 'symbols.id')
+SYM = {
+    'all': os.path.join(SYMDIR, 'symbols.all'),
+    'sh':  os.path.join(SYMDIR, 'symbols.sh'),
+    'sz':  os.path.join(SYMDIR, 'symbols.sz'),
+    'cy':  os.path.join(SYMDIR, 'symbols.cy'),
+    'id':  os.path.join(SYMDIR, 'symbols.id'),
+}
 
-def get_stock_symbol(type):
-    pass
+INDEX = {
+    'sh': 'sh000001',
+    'sz': 'sz399001',
+    'cy': 'sz399006',
+}
 
-def get_index_symbol():
-    pass
+def get_stock_symbol(type='all'):
+    global SYM
+    if type not in SYM:
+        raise InvalidType('Please specify the type: all|sh|sz|cy|id')
+    filename = SYM[type]
+    f = open(filename, "r")
+    content = f.read()
+    f.close()
+    symbols = content.split('\n')
+    return symbols
+
+def get_index_symbol(type):
+    global INDEX
+    return INDEX[type]
 
 def download_symbols():
     base_url = "http://stock.gtimg.cn/data/index.php?appn=rank&t=ranka/chr&o=0&l=40&v=list_data"
@@ -25,16 +43,42 @@ def download_symbols():
     i = 1
     while True:
         url = base_url + "&p=" + str(i)
-        print url
         result = request.send_request(url)
         pattern = re.compile('data:\'(.*)\'')
         s = pattern.search(result)
         gsymbols = s.group(1).split(',')
         symbols = symbols + gsymbols
-        print gsymbols
         if len(gsymbols) < 40:
-            print "ending"
             break
 
         i = i + 1
-    print symbols
+
+    global SYM, INDEX
+    sh_symbols = []
+    sz_symbols = []
+    cy_symbols = []
+
+    p_sh = re.compile('^sh')
+    p_sz = re.compile('^sz0')
+    p_cy = re.compile('^sz3')
+    for s in symbols:
+        if p_sh.search(s) and s != INDEX['sh']:
+            sh_symbols.append(s)
+        elif p_sz.search(s) and s != INDEX['sz']:
+            sz_symbols.append(s)
+        elif p_cy.search(s) and s != INDEX['cy']:
+            cy_symbols.append(s)
+
+    f_all = open(SYM['all'], "w")
+    f_sh = open(SYM['sh'], "w")
+    f_sz = open(SYM['sz'], "w")
+    f_cy = open(SYM['cy'], "w")
+    f_all.write('\n'.join(symbols))
+    f_sh.write('\n'.join(sh_symbols))
+    f_sz.write('\n'.join(sz_symbols))
+    f_cy.write('\n'.join(cy_symbols))
+    f_all.close()
+    f_sh.close()
+    f_sz.close()
+    f_cy.close()
+
--- test/test_utils_symbol_util.py
+++ test/test_utils_symbol_util.py
@@ -1,9 +1,20 @@
 from stock.utils.symbol_util import *
+import unittest
 import os
 
-class TestRequest:
-    def setup(self):
+class TestRequest(unittest.TestCase):
+    def setUp(self):
         pass
+
+    def test_get_stock_symbol(self):
+        symbols = get_stock_symbol()
+        self.assertTrue(len(symbols) > 0)
+        symbols = get_stock_symbol('sh')
+        self.assertTrue(len(symbols) > 0)
+    
+    def test_get_index_symbol(self):
+        index = get_index_symbol('sh')
+        self.assertEqual(index, 'sh000001')
 
     def test_download_symbols(self):
         download_symbols()
