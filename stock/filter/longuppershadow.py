df0fc51c976bdd83ce9a68fede64ddb7805fb033
Zhongqiang Shen <zqshen.1985@gmail.com>
2014-10-06 21:35:24 +0800

    modify long upper shadow filter

--- stock/filter/longuppershadow.py
+++ stock/filter/longuppershadow.py
@@ -8,25 +8,28 @@ class LongUpperShadow(Filter):
             if isinstance(self.marketdata, realtimedata.RealTimeData):
                 bar_today = self.marketdata.get_data(exsymbol)
                 history = self.marketdata.get_history_by_date(exsymbol)
-                bar_yest = history[0]
+                bar1 = history[0]
+                bar2 = history[1]
             elif isinstance(self.marketdata, backtestdata.BackTestData):
                 history = self.marketdata.get_history_by_date(exsymbol)
                 bar_today = history[0]
-                bar_yest = history[1]
+                bar1 = history[1]
+                bar2 = history[2]
 
             vol = bar_today.volume
-            zt_price = round(bar_yest.close * 1.1 * 100) / 100
+            zt_price = round(bar1.close * 1.1 * 100) / 100
             if vol == 0:
                 return
 
             if zt_price == bar_today.close:
                 return
 
-            chgperc = (bar_today.close / bar_yest.close - 1) * 100
-            is_high = bar_today.high > bar_today.open * 1.06
+            chgperc = (bar_today.close / bar1.close - 1) * 100
+            chg_yest = bar1.close / bar2.close - 1
+            is_high = bar_today.high > bar_today.open * 1.04
             small_body = abs(bar_today.close - bar_today.open) < \
                 bar_today.open * 0.02
-            if is_high and small_body:
+            if chg_yest > 0.06 and is_high and small_body:
                 self.output.append(CheckResult(exsymbol, chgperc=chgperc, \
                     pe=bar_today.pe, cvalue=bar_today.cvalue, value=bar_today.value))
         except Exception, e:
