4ce023a20cab37fe855b7feee42ac51902d046d3
Zhongqiang Shen <zqshen.1985@gmail.com>
2015-01-28 21:08:39 +0800

    power filter for realtime

---
+++ jobs/powerfilter_realtime.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import threading
+from stock.globalvar import *
+from stock.powerfilter import *
+from stock.marketdata import *
+from jinja2 import Environment, FileSystemLoader
+import stock.utils.symbol_util
+import os.path
+import sys
+import logging
+import logging.config
+import datetime
+
+if not os.path.isdir(OUTDIR):
+    os.makedirs(OUTDIR)
+
+logging.config.fileConfig(LOGCONF)
+logger = logging.getLogger(__name__)
+
+dt = datetime.datetime.today()
+date = dt.strftime("%y%m%d")
+lock = threading.RLock()
+marketdata = realtimedata.RealTimeData(lock=lock)
+
+if not os.path.isdir(OUTDIR):
+    os.makedirs(OUTDIR)
+
+filters = [
+    longlowershadow.LongLowerShadow,
+]
+
+result = {}
+exsymbols = stock.utils.symbol_util.get_stock_symbols('all')
+for f in filters:
+    fname = f.__name__
+    output = f(marketdata).check(exsymbols)
+    result[fname] = output
+
+env = Environment(loader=FileSystemLoader(TMPLDIR))
+template = env.get_template('stock_list.tmpl')
+html = template.render(stocks=result)
+outfile = os.path.join(OUTDIR, 'power_realtime_' + date + '.html')
+with open(outfile, 'w') as f:
+    f.write(html)
