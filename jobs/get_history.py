94b81a21e673b10153f2d606eed2346fb82541c2
Zhongqiang Shen <shenzhongqiang@msn.com>
2017-06-14 05:33:55 -0700

    use multi-process instead of multi-thread

--- jobs/get_history.py
+++ jobs/get_history.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os.path
 import threading
-import Queue
+from multiprocessing import Pool
 from stock.utils import request
 import stock.utils.symbol_util
 from stock.globalvar import *
@@ -15,23 +15,11 @@ if not os.path.isdir(stock_dir):
 if not os.path.isdir(index_dir):
     os.makedirs(index_dir)
 
-class Downloader(threading.Thread):
-    def __init__(self, queue):
-        threading.Thread.__init__(self)
-        self.queue = queue
-
-    def run(self):
-        while True:
-            try:
-                data = self.queue.get()
-                print data["symbol"]
-                self.download_stock_history(data["symbol"], is_index=data["is_index"])
-                self.queue.task_done()
-            except Exception, e:
-                print str(e)
-                self.queue.task_done()
-
-    def download_stock_history(self, symbol, is_index):
+def download_stock_history(data):
+    try:
+        symbol = data["symbol"]
+        print "getting history for %s" % symbol
+        is_index = data["is_index"]
         df = ts.get_k_data(symbol, index=is_index)
         path = None
         if not is_index:
@@ -39,22 +27,21 @@ class Downloader(threading.Thread):
         else:
             path = os.path.join(index_dir, symbol)
         df.to_csv(path)
+    except Exception, e:
+        print "error getting history due to %s" % str(e)
 
 if __name__ == "__main__":
-    queue = Queue.Queue()
-    for i in range(10):
-        t = Downloader(queue)
-        t.setDaemon(True)
-        t.start()
+    pool = Pool(20)
 
     # download stock symbols
     symbols = stock.utils.symbol_util.get_stock_symbols()
     index_symbols = stock.utils.symbol_util.get_index_symbols()
+    all_symbols = []
     for symbol in symbols:
-        queue.put({"symbol": symbol, "is_index": False})
+        all_symbols.append({"symbol": symbol, "is_index": False})
 
     for symbol in index_symbols:
-        queue.put({"symbol": symbol, "is_index": True})
+        all_symbols.append({"symbol": symbol, "is_index": True})
 
-    queue.join()
+    pool.map(download_stock_history, all_symbols)
 
