46303296ee3eb6dfa347974320c98e16a7b21364
Zhongqiang Shen <shenzhongqiang@msn.com>
2017-07-17 03:12:03 -0700

    load correct strategy

--- jobs/find_optimum_grid.py
+++ jobs/find_optimum_grid.py
@@ -2,14 +2,14 @@ import logging
 import numpy as np
 from stock.optimize.grid_search import gs
 import stock.trade.report
-import stock.strategy.ema
+import stock.strategy.volup
 
 logger = logging.getLogger("jobs.find_optimum_grid")
 
 start = '2016-07-01'
 end = '2017-06-30'
 def sample_loss(param):
-    strategy = stock.strategy.ema.EmaStrategy(start, end,
+    strategy = stock.strategy.volup.VolupStrategy(start, end,
         **param)
     strategy.run()
     report = stock.trade.report.Report()
--- stock/optimize/grid_search/gs.py
+++ stock/optimize/grid_search/gs.py
@@ -5,12 +5,12 @@ def search(sample_loss, param_grid):
     params = ParameterGrid(param_grid)
     print "Total combinations: %d" % (len(params))
     pool = Pool(20)
-    result = []
     async_res = []
     for param in params:
         res = pool.apply_async(sample_loss, (param,))
         async_res.append({"param": param, "res": res})
 
+    result = []
     for i in range(len(async_res)):
         res = async_res[i]["res"]
         param = async_res[i]["param"]
