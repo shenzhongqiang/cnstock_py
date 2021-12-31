23772b287766e7f7b4a5e02f01085689031494ab
Zhongqiang Shen <linushen@ebay.com>
2021-12-31 15:47:59 +0800

    merge get concepts, get industries functions to download.py

--- jobs/download.py
+++ jobs/download.py
@@ -2,20 +2,13 @@ import re
 import sys
 import argparse
 import os.path
-import urllib
 
 from multiprocessing import Pool
-from urllib.parse import urlsplit, parse_qs, urlencode
-
 from tqdm import trange
 import requests
-import tushare as ts
 import pandas as pd
 
-from stock.utils import request
-import stock.utils.symbol_util
-from stock.globalvar import HIST_DIR, SYM
-from stock.marketdata.storefactory import get_store
+from stock.globalvar import HIST_DIR, SYM, BASIC_DIR
 
 def is_sh(a_symbol):
     m = re.match(r"6", a_symbol)
@@ -119,10 +112,121 @@ def download_hist():
         res = results[i]
         res.wait()
 
+
+def download_concepts():
+    pz = 500
+    i = 1
+    result = []
+    while True:
+        url = "https://11.push2.eastmoney.com/api/qt/clist/get?pn={}&pz={}&po=1&np=1&fltt=2&invt=2&fid=f3&fs=m:90+t:3+f:!50&fields=f12,f14".format(i, pz)
+        r = requests.get(url, verify=False)
+        data = r.json()
+        total = data["data"]["total"]
+        items = data["data"]["diff"]
+        result.extend(items)
+        if (i-1)*pz+len(items) >= total:
+            break
+        i += 1
+    result = list(map(lambda x: {"symbol": x["f2"], "name": x["f14"]}, result))
+    return result
+
+def download_industries():
+    pz = 500
+    i = 1
+    result = []
+    while True:
+        url = "https://24.push2.eastmoney.com/api/qt/clist/get?pn={}&pz={}&po=1&np=1&fltt=2&invt=2&fid=f3&fs=m:90+t:2+f:!50&fields=f12,f14".format(i, pz)
+        r = requests.get(url, verify=False)
+        data = r.json()
+        total = data["data"]["total"]
+        items = data["data"]["diff"]
+        result.extend(items)
+        if (i-1)*pz+len(items) >= total:
+            break
+        i += 1
+    result = list(map(lambda x: {"symbol": x["f12"], "name": x["f14"]}, result))
+    return result
+
+def download_concept_stocks(concept_id):
+    pz = 500
+    i = 1
+    result = []
+    while True:
+        url = "https://push2.eastmoney.com/api/qt/clist/get?fid=f62&pn={}&po=1&pz={}&np=1&fltt=2&invt=2&fs=b%3A{}&fields=f12,f14".format(i, pz, concept_id)
+        r = requests.get(url, verify=False)
+        data = r.json()
+        total = data["data"]["total"]
+        items = data["data"]["diff"]
+        result.extend(items)
+        if (i-1)*pz+len(items) >= total:
+            break
+        i += 1
+    result = list(map(lambda x: {"symbol": x["f12"], "name": x["f14"]}, result))
+    return result
+
+def download_industry_stocks(industry_symbol):
+    pz = 500
+    i = 1
+    result = []
+    while True:
+        url = "https://push2.eastmoney.com/api/qt/clist/get?fid=f62&po={}&pz={}&pn=1&np=1&fltt=2&invt=2&fs=b%3A{}&fields=f12,f14".format(i, pz, industry_symbol)
+        r = requests.get(url, verify=False)
+        data = r.json()
+        total = data["data"]["total"]
+        items = data["data"]["diff"]
+        result.extend(items)
+        if (i-1)*pz+len(items) >= total:
+            break
+        i += 1
+    result = list(map(lambda x: {"symbol": x["f12"], "name": x["f14"]}, result))
+    return result
+
+def download_all_concepts_stocks():
+    concepts = download_concepts()
+    s_concept_symbol = []
+    s_concept_name = []
+    s_symbol = []
+    s_name = []
+    for concept in concepts:
+        concept_name = concept["name"]
+        concept_symbol = concept["symbol"]
+        stocks = download_concept_stocks(concept_symbol)
+        symbols = list(map(lambda x: x["symbol"], stocks))
+        names = list(map(lambda x: x["name"], stocks))
+        s_concept_symbol.extend([concept_symbol]*len(stocks))
+        s_concept_name.extend([concept_name]*len(stocks))
+        s_symbol.extend(symbols)
+        s_name.extend(names)
+    df = pd.DataFrame({"concept_symbol": s_concept_symbol, "concept_name": s_concept_name, "symbol": s_symbol, "name": s_name})
+    filepath = os.path.join(BASIC_DIR, "concept")
+    df.to_csv(filepath, index=False)
+
+def download_all_industries_stocks():
+    industries = download_industries()
+    s_industry_symbol = []
+    s_industry_name = []
+    s_symbol = []
+    s_name = []
+    for industry in industries:
+        industry_name = industry["name"]
+        industry_symbol = industry["symbol"]
+        stocks = download_industry_stocks(industry_symbol)
+        symbols = list(map(lambda x: x["symbol"], stocks))
+        names = list(map(lambda x: x["name"], stocks))
+        s_industry_symbol.extend([industry_symbol]*len(stocks))
+        s_industry_name.extend([industry_name]*len(stocks))
+        s_symbol.extend(symbols)
+        s_name.extend(names)
+    df = pd.DataFrame({"industry_symbol": s_industry_symbol, "industry_name": s_industry_name, "symbol": s_symbol, "name": s_name})
+    filepath = os.path.join(BASIC_DIR, "industry")
+    df.to_csv(filepath, index=False)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--symbol", action="store_true", default=False)
     parser.add_argument("--hist", action="store_true", default=False)
+    parser.add_argument("--concept", action="store_true", default=False)
+    parser.add_argument("--industry", action="store_true", default=False)
     args = parser.parse_args()
     if args.symbol:
         download_stock_symbols()
@@ -130,4 +234,10 @@ if __name__ == "__main__":
         sys.exit(0)
     if args.hist:
         download_hist()
+        sys.exit(0)
+    if args.concept:
+        download_all_concepts_stocks()
+        sys.exit(0)
+    if args.industry:
+        download_all_industries_stocks()
         sys.exit(0)
